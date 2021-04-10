03b2371def6ea74ecda386a489305ba5e7554570
shintoo <seanprapp@gmail.com>
2021-04-09 22:05:13 -0400

    Mentions handlers

--- cleardarkskybot.py
+++ cleardarkskybot.py
@@ -10,6 +10,7 @@ Files:
 """
 
 import csv
+import json
 from datetime import datetime
 import time
 import requests
@@ -40,6 +41,17 @@ def read_locations():
 
    return locations
 
+def add_to_locations(key, title):
+    locations = read_locations()
+    if [title, key] in locations: # Gross I know, eventually will make locations a dict
+        return -1
+
+    with open("tables/locations.csv", "a") as f:
+        f.write(f"{title},{key}")
+
+def build_key_url(key):
+    return f"https://www.cleardarksky.com/c/{key}key.html"
+
 def tweet_body(location_row):
     args = {
       "location_name": location_row[0],
@@ -51,6 +63,7 @@ def tweet_body(location_row):
         greeting = greetings[random.randint(0, len(greetings)-1)]
 
     args["greeting"] = greeting
+    args["key_url"] = build_key_url(location_row[1])
 
     with open("templates/tweet.j2", "r") as f:
         body = Template(f.read()).render(**args)
@@ -96,48 +109,119 @@ def read_last_mention_handled_id():
     return tweet_id
 
 
+def update_last_mention_handled_id(mention_id):
+    with open("last_mention_handled.txt", "w") as f:
+        f.write(str(mention_id))
+
+def coordinates_from_query(query):
+    with open("tables/positionstack.txt", "r") as f:
+        key = f.read().strip()
+
+    query_url = f"http://api.positionstack.com/v1/forward?access_key={key}&query={query}"
+    print(f"retrieving: {query_url}")
+    response = requests.get(query_url, headers={"User-Agent": user_agent})
+    response_dict = json.loads(response.text)
+    first = response_dict["data"][0]
+    latitude = first["latitude"]
+    longitude = first["longitude"] * -1 # W
+
+    return latitude, longitude
+
 def find_key_from_query(query):
     """Find CSK key based off of plaintext query"""
 
-    query_url = f"https://www.cleardarksky.com/cgi-bin/find_chart.py?keys={'+'.join(query)}&type=text&Mn=Solar%2520Power&doit=Find"
+
+    latitude, longitude = coordinates_from_query(query)
+
+    query_url = f"https://www.cleardarksky.com/cgi-bin/find_chart.py?type=llmap&Mn=astrophysics&olat={latitude}&olong={longitude}&olatd=&olatm=&olongd=&olongm=&unit=1"
+
+#    query_url = f"https://www.cleardarksky.com/cgi-bin/find_chart.py?keys={query}&type=text&Mn=Solar%2520Power&doit=Find"
     
-    response = requests.get(url, headers={"User-Agent": user_agent}) 
+    response = requests.get(query_url, headers={"User-Agent": user_agent}) 
     if response.status_code != 200:
         print(f"Error getting {query_url}: response: {response.status_code}")
         return
 
-    # TODO 
-    # get html from response
-    # find line with key on it (has key.html in line)
-    # grab key from line ("../c/(.*)key.html")
+    pre_trim = response.text[response.text.index("../c/")+5:]
+    post_trim = pre_trim[:pre_trim.index("key.html")]
+    return post_trim
 
 
 def find_title_from_key(key):
-    # TODO
-    # get page using key
-    # title is in <title> (e.g. <title>Joey's Observator Clear Sky Chart</title>)
-
+    url = f"https://www.cleardarksky.com/c/{key}key.html"
+    response = requests.get(url, headers={"User-Agnet": user_agent})
+    text = response.text
+    try:
+        title_tag = text.index("<title>")
+        clear = text.index("Clear")
+        chart_title = response.text[title_tag+7:clear-1]
+    except Exception as e:
+        print(f"Error finding title: {e}\nresponse:\n{text}\n")
+        return None
+    return chart_title
 
 def handle_add(api, query, tweet_id):
-    key = find_key_from_query(query)
-    title = find_title_from_key(key)
-    add_to_locations_file(key, title) 
+    key = None
+    title = None
+
+    print(f"Handling add for query '{query}'")
+
+    while not key:
+        key = find_key_from_query(query)
+        if not key:
+            print("Bad response getting key, trying again")
+            time.sleep(5)
+    print(f"Got key: {key}")
+
+    while not title:
+        title = find_title_from_key(key)
+        if not title:
+            print("Bad response getting title, trying again")
+            time.sleep(5)
+
+    print(f"Got title: {title}")
+
+    if add_to_locations(key, title) == -1:
+        body = f"{title} was the closest found location with Clear Sky Charts available, and it is already being published."
+    else:
+        body = f"{title} has been added to the list!"
+
+    print(f"Posting tweet:\n{body}")
 
     api.update_status(
-        status=f"{title} has been added to the list!",
+        status=body,
         in_reply_to_status_id=tweet_id
     )
 
 def handle_show(api, query, tweet_id):
-    key = find_key_from_query(query)
-    chart = download_chart(key)
-    title = find_title_from_key(key)
+    key = None
+    title = None
 
+    print(f"Handling show for query '{query}'")
+
+    while not key:
+        key = find_key_from_query(query)
+        if not key:
+            print("Bad response getting key, trying again")
+            time.sleep(5)
+    print(f"Got key: {key}")
+
+    while not title:
+        title = find_title_from_key(key)
+        if not title:
+            print("Bad response getting title, trying again")
+            time.sleep(5)
+
+    chart = download_chart(key)
+    infotext = f"How to read this chart: {build_key_url(key)}"
+    print(f"Posting tweet:\nHere is the Clear Sky Chart for {title}.\n\n{infotext}\n<file: {chart}>")
+    
     api.update_with_media(
-        status=f"Here is the Clear Sky Chart for {title}",
+        status=f"Here is the Clear Sky Chart for {title}.\n\n{infotext}",
         in_reply_to_status_id=tweet_id,
         filename=chart
     )
+
 
 def daily_loop(api, time_to_start):
     locations = read_locations()
@@ -154,25 +238,35 @@ def daily_loop(api, time_to_start):
         print("Sleeping for 24h...")
         time.sleep(24 * 60 * 60)
 
-def mentions_handler(api, since_id):
+def mentions_handler(api):
 
-    while True:
-        new_since_id = since_id
+    since_id = read_last_mention_handled_id()
 
-        for tweet in tweepy.Cursor(api.mentions_timeline, since_id=new_since_id).items():
-            new_since_id = max(tweet.id, new_since_id)
+    try:
+        while True:
+            new_since_id = since_id
 
-            tokens = tweet.text.lower().split()
-        
-            if "add" in tokens:
-                query = tokens[tokens.index("add")+1:]
-                handle_add(api, query, tweet.id)
-            else if "show" in tokens:
-                query = tokens[tokens.index("show")+1:]
-                handle_show(api, query, tweet.id)
+            for tweet in tweepy.Cursor(api.mentions_timeline, since_id=new_since_id).items():
+                new_since_id = max(tweet.id, new_since_id)
 
-        time.sleep(10)
-        # TODO write new_since_id to file if it has changed
+
+
+                print(f"Received mention:\n'{tweet.text}'\n")
+
+                text = tweet.text.lower()
+                tweet_id = 5
+                if "add" in text:
+                    query = text[text.index("add")+4:]
+                    handle_add(api, query, tweet.id)
+                elif "show" in text:
+                    query = text[text.index("show")+5:]
+                    handle_show(api, query, tweet.id)
+
+            update_last_mention_handled_id(new_since_id)
+            time.sleep(10)
+    except KeyboardInterrupt as e:
+        update_last_mention_handled_id(new_since_id)
+
 
 if __name__ == "__main__":
     api = twitter_api()
@@ -180,8 +274,8 @@ if __name__ == "__main__":
 
     last_mention_handled = read_last_mention_handled_id()
 
-    daily_post_thread = Thread(target=daily_loop, args=(api, 5,))
-    mentions_handler_thread = Thread(target=mentions_handler, args=(api, last_mention_handled,))
+    daily_post_thread = Thread(target=daily_loop, args=(api, 17,))
+    mentions_handler_thread = Thread(target=mentions_handler, args=(api,))
 
     daily_post_thread.start()
     mentions_handler_thread.start()
